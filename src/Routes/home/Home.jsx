7dd4cca7ffe5733c97fb61027d27f9da472b170e
Iyiola Abdulmalik <abdulmalikiyiola@gmail.com>
2025-09-19 04:33:54 -0700

    fourth commit

--- src/Routes/home/Home.jsx
+++ src/Routes/home/Home.jsx
@@ -3,7 +3,7 @@ import TopTen from './TopTen';
 
 function Home() {
     return (
-        <div className='absolute top-0 left-0 w-full h-[82vh] max-[768px]:h-[69vh]'>
+        <div className='absolute top-0 left-0 w-full h-[82vh] max-[768px]:h-[6vh]'>
           
           <LandPage />
             <div>
