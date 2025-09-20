602d5ecdd7c7e29b60688350d7b2912362edd518
Iyiola Abdulmalik <abdulmalikiyiola@gmail.com>
2025-09-20 16:09:29 -0700

    sixth commit

--- src/Routes/home/TopTen.jsx
+++ src/Routes/home/TopTen.jsx
@@ -11,8 +11,8 @@ function TopTen() {
 
     const settings = {
         infinite: true,
-        speed: 1000,
-        slidesToShow: 5,
+        speed: 400,
+        slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
             {
